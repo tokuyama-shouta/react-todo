24273f549dcdb2da96f1cf07a9c077a51f1c8cf0
tokuyama shouta <toku1031ts@gmail.com>
2020-11-02 22:41:08 +0900

    task delete

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import Form from './Form'
 import Todo from './Todo'
+import CheckAll from './CheckAll'
 
 let currentId = 0;
 
@@ -16,14 +17,12 @@ class App extends React.Component{
     }
   }
   render() {
+      const { todos } = this.state
       return (
         <div>
           <Form onSubmit={this.handleSubmit}/>
-
-          <label>
-            <input type="checkbox"/>
-            全て完了にする
-          </label>
+          <CheckAll 
+            allCompleted={todos.length > 0 && todos.every(({completed}) => completed)} onChange={this.handleChangeAllCompleted}/>
 
           <select>
             <option>全て</option>
@@ -32,10 +31,19 @@ class App extends React.Component{
           </select>
 
           <ul>
-            {this.state.todos.map(({ id,text }) => <li key={id}><Todo text={text}/></li>)}
+            {this.state.todos.map(({ id,text,completed }) => 
+              <li key={id}>
+                  <Todo 
+                    id={id}
+                    text={text} 
+                    completed={completed} 
+                    onChange={this.handleChangeCompleted}
+                  />
+              </li>
+            )}
           </ul>
 
-          <button>完了済みを全て消去</button>
+          <button onClick={this.handleClickDleteCompleted}>完了済みを全て消去</button>
 
         </div>
       )
@@ -45,11 +53,42 @@ class App extends React.Component{
     const newTodo = {
       id: currentId,
       text,
+      completed: false
     }
     const newTodos = [...this.state.todos,newTodo]
     //1つ1つ取り出して新しい配列に追加していく
     this.setState({ todos: newTodos})
     currentId++;
+  }
+
+  handleChangeAllCompleted = completed => {
+    const newTodos = this.state.todos.map(todo => ({
+        ...todo,
+        completed,
+      
+    }));
+
+    this.setState({ todos:newTodos})
+  }
+
+  handleChangeCompleted = (id,completed) => {
+    const newTodos = this.state.todos.map(todo => {
+        if(todo.id === id) {
+          return{
+            ...todo,
+            completed,
+
+          }
+        }
+
+        return todo
+    })
+    this.setState({ todos: newTodos });
+  }
+
+  handleClickDleteCompleted = () => {
+    const newTodos = this.state.todos.filter(({ completed}) => !completed)
+    this.setState({todos: newTodos})
   }
 }
 
